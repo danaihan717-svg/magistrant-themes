a8cc3ea95289f9f6b65e16674d102af4bb08bab0
danaihan717-svg <danaihan717@gmail.com>
2025-10-02 10:33:01 +0500

    Update centers.js

--- centers.js
+++ centers.js
@@ -1,17 +1,41 @@
 module.exports = [
   {
-    name: { kk: "Мемлекеттік-құқықтық пәндер орталығы", ru: "Центр государственного и правового права" },
-    maxStudents: 31,
-    topics: Array.from({length:7}, (_,i)=>({id:i+1, title:{kk:`Тақырып ${i+1} (МҚ)`, ru:`Тема ${i+1} (ГП)`}, student:null, time:null}))
+    name: { kk: "Мемлекеттік-құқықтық пәндердің ғылыми-білім беру орталығы", ru: "Научно-образовательный центр гос.-правовых дисциплин" },
+    maxStudents: 30,
+    topics: [
+      { id: 1, title: { kk: "Конституциялық құқық", ru: "Конституционное право" }, student: null, time: null },
+      { id: 2, title: { kk: "Мемлекеттік басқару", ru: "Государственное управление" }, student: null, time: null },
+      { id: 3, title: { kk: "Құқықтық мемлекет", ru: "Правовое государство" }, student: null, time: null },
+      { id: 4, title: { kk: "Мемлекеттік қызмет", ru: "Государственная служба" }, student: null, time: null },
+      { id: 5, title: { kk: "Азаматтық және саяси құқықтар", ru: "Гражданские и политические права" }, student: null, time: null },
+      { id: 6, title: { kk: "Сайлау құқығы", ru: "Избирательное право" }, student: null, time: null },
+      { id: 7, title: { kk: "Құқықтық этика", ru: "Правовая этика" }, student: null, time: null }
+    ]
   },
   {
-    name: { kk: "Азаматтық-құқықтық пәндер орталығы", ru: "Центр гражданского права" },
-    maxStudents: 31,
-    topics: Array.from({length:7}, (_,i)=>({id:i+1, title:{kk:`Тақырып ${i+1} (АҚ)`, ru:`Тема ${i+1} (ГП)`}, student:null, time:null}))
+    name: { kk: "Азаматтық-құқықтық пәндердің ғылыми-білім беру орталығы", ru: "Научно-образовательный центр гражданско-правовых дисциплин" },
+    maxStudents: 30,
+    topics: [
+      { id: 1, title: { kk: "Азаматтық құқық", ru: "Гражданское право" }, student: null, time: null },
+      { id: 2, title: { kk: "Келісім-шарттық құқық", ru: "Договорное право" }, student: null, time: null },
+      { id: 3, title: { kk: "Мүліктік құқық", ru: "Имущественное право" }, student: null, time: null },
+      { id: 4, title: { kk: "Авторлық құқық", ru: "Авторское право" }, student: null, time: null },
+      { id: 5, title: { kk: "Отбасы құқығы", ru: "Семейное право" }, student: null, time: null },
+      { id: 6, title: { kk: "Мұрагерлік құқығы", ru: "Наследственное право" }, student: null, time: null },
+      { id: 7, title: { kk: "Коммерциялық құқық", ru: "Коммерческое право" }, student: null, time: null }
+    ]
   },
   {
-    name: { kk: "Қылмыстық-құқықтық пәндер орталығы", ru: "Центр уголовного права" },
-    maxStudents: 31,
-    topics: Array.from({length:7}, (_,i)=>({id:i+1, title:{kk:`Тақырып ${i+1} (ҚҚ)`, ru:`Тема ${i+1} (УП)`}, student:null, time:null}))
+    name: { kk: "Қылмыстық-құқықтық пәндердің ғылыми-білім беру орталығы", ru: "Научно-образовательный центр уголовно-правовых дисциплин" },
+    maxStudents: 30,
+    topics: [
+      { id: 1, title: { kk: "Қылмыстық құқық негіздері", ru: "Основы уголовного права" }, student: null, time: null },
+      { id: 2, title: { kk: "Қылмыстық жауапкершілік", ru: "Уголовная ответственность" }, student: null, time: null },
+      { id: 3, title: { kk: "Қылмыстық іс жүргізу", ru: "Уголовное судопроизводство" }, student: null, time: null },
+      { id: 4, title: { kk: "Сот сараптамалары", ru: "Судебные экспертизы" }, student: null, time: null },
+      { id: 5, title: { kk: "Жеңіл және ауыр қылмыстар", ru: "Легкие и тяжкие преступления" }, student: null, time: null },
+      { id: 6, title: { kk: "Құқық қорғау органдары", ru: "Органы правопорядка" }, student: null, time: null },
+      { id: 7, title: { kk: "Қылмыстық профилактика", ru: "Уголовная профилактика" }, student: null, time: null }
+    ]
   }
 ];
